fa3882765b08a83c30dae8131978572b36876682
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-10-11 11:33:21 +0530

    Added humadity and wind speed  in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=3ef9d7149d07f0e31705a7d07d304015&query='+ latitude +','+ longitude +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=3ef9d7149d07f0e31705a7d07d304015&query='+ latitude +','+ longitude// +'&units=f'
 
     request({url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree celsius out. It feels like ' + body.current.feelslike + ' degree celsius out. The humidity is '+ body.current.humidity +'% and the wind speed is ' + body.current.wind_speed + 'mph.')
         }     
     })
 }
