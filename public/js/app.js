9c7ca3fa1e44635434a99ee98240df5d25c6fba0
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-10-14 12:08:04 +0530

    code formatting

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
-                messageOne.textContent =data.error
+                messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=3ef9d7149d07f0e31705a7d07d304015&query='+ latitude +','+ longitude// +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=3ef9d7149d07f0e31705a7d07d304015&query=' + latitude + ',' + longitude// +'&units=f'
 
     request({url, json: true}, (error, { body }) => {
         if (error) {
